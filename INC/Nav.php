d1fbc9e574052183a08dfd9cac7c4d6cb8f4e0c6
Regina Hong <sunghee.regina.hong@gmail.com>
2018-06-17 20:10:36 -0400

    Navigation update
    Top menu updated to include Verb System page

--- INC/Nav.php
+++ INC/Nav.php
@@ -14,6 +14,7 @@
         <li><a href="TheBibleColorCoded.php"><i class="fas fa-book fa-lg"></i> Color Coded Bible</a></li>
         <li><a href="HebrewStrong.php"><i class="fas fa-book fa-lg"></i> Dictionary</a></li>
         <li><a href="AncientBiblicalHebrewGrammarStudy.php"><i class="fas fa-pencil-alt"></i> Grammar Study</a></li>
+        <li><a href="BiblicalHebrewVerbSystem.php"><i class="fas fa-pencil-alt"></i> Verb System</a></li>
         <li><a href="StudyResources.php"><i class="fas fa-paperclip fa-lg"></i> Study Resources</a></li>
         <li><a href="Analysis.php"><i class="fas fa-chart-bar fa-lg"></i> Analysis</a></li>
         <li><a href="https://blessedregina.org/blog" target="_blank"><i class="fab fa-wordpress fa-lg"></i> Blog</a></li>
